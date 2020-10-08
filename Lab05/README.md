e1d1961de206af9426c9de9b6dd2cadda3f166c9
Ubuntu <baasioonii2015@gmail.com>
2020-10-08 23:30:04 +0000

    sourceCodes

---
+++ Lab05/README.md
@@ -0,0 +1,1 @@
+to use the make command you can replace repeat that's in the Makefile with the program you would like to run and then type down makerun + the name of your program
---
+++ Lab05/repeat.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class repeat{
+
+
+    public static void main(String[] args) {
+     Scanner keyboard = new Scanner(System.in);
+     System.out.println("Enter the text you would like to have printed out");
+     String Text = keyboard.nextLine();
+     System.out.println(Text);
+    }
+    
+}
+
+
